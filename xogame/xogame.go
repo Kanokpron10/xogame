410a2b77345cab6745685dfd3648e0bc9024d25c
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-22 14:28:48 +0700

    [Edited]: แก้ไขฟังก์ชัน marking ในไฟล์ xogame.go

--- xogame/xogame.go
+++ xogame/xogame.go
@@ -1,7 +1,7 @@
 package xogame
 
 type Game struct {
-	board  [3][3]string
+	Board  [3][3]string
 	player []Player
 	turn   string
 }
@@ -14,7 +14,7 @@ type Player struct {
 
 func NewGame(player1, player2 Player, turn string) Game {
 	return Game{
-		board:  [3][3]string{},
+		Board:  [3][3]string{},
 		player: []Player{player1, player2},
 		turn:   turn,
 	}
@@ -28,15 +28,15 @@ func NewPlayer(name, symbol string) Player {
 	}
 }
 
-func (game Game) Play(player Player, row, column int) string {
+func (game *Game) Play(player Player, row, column int) string {
 	game.marking(player, row, column)
 	winner := game.checkWin()
 	game.switchTurn()
 	return winner
 }
 
-func (game Game) marking(player Player, row, column int) {
-
+func (game *Game) marking(player Player, row, column int) {
+	game.Board[row][column] = player.symbol
 }
 
 func (game Game) checkWin() string {
