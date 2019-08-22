3b9d8a61eded4a681934fcd64adbd1fae8a580d9
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-22 23:22:33 +0700

    [Added]: เพิ่มเทส Test_PlayerOne_Win_Vertical_FirstLine ในไฟล์ xogame_test.go

--- xogame/xogame_test.go
+++ xogame/xogame_test.go
@@ -5,7 +5,7 @@ import (
 	"xogame/xogame"
 )
 
-func Test_PlayerOne_Win_Horizontal_FirstLine_(t *testing.T) {
+func Test_PlayerOne_Win_Horizontal_FirstLine(t *testing.T) {
 	expected := "x WIN Horizontal"
 	player1 := xogame.NewPlayer("Mo", "x")
 	player2 := xogame.NewPlayer("Praw", "o")
@@ -50,6 +50,23 @@ func Test_PlayerOne_Win_Horizontal_ThirdLine(t *testing.T) {
 	game.Play(player1, 2, 1)
 	game.Play(player2, 0, 1)
 	actual := game.Play(player1, 2, 2)
+
+	if expected != actual {
+		t.Errorf("Expect %s but got %s", expected, actual)
+	}
+}
+
+func Test_PlayerOne_Win_Vertical_FirstLine(t *testing.T) {
+	expected := "x WIN Vertical"
+	player1 := xogame.NewPlayer("Mo", "x")
+	player2 := xogame.NewPlayer("Praw", "o")
+	game := xogame.NewGame(player1, player2, "x")
+
+	game.Play(player1, 0, 0)
+	game.Play(player2, 2, 0)
+	game.Play(player1, 1, 0)
+	game.Play(player2, 1, 1)
+	actual := game.Play(player1, 2, 0)
 
 	if expected != actual {
 		t.Errorf("Expect %s but got %s", expected, actual)
