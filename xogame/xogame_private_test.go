7af5b51d412bab7259eb07e878e933c0881921f3
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-23 00:30:02 +0700

    [Added]: เพิ่มเทส Test_CheckFullBoard_Should_Be_True ในไฟล์ xogame_private_test.go

--- xogame/xogame_private_test.go
+++ xogame/xogame_private_test.go
@@ -95,3 +95,25 @@ func Test_SwitchTurn_Should_Be_X(t *testing.T) {
 		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
+
+func Test_CheckFullBoard_Should_Be_True(t *testing.T) {
+	expected := true
+	player1 := NewPlayer("Mo", "x")
+	player2 := NewPlayer("Praw", "o")
+	game := NewGame(player1, player2, "x")
+
+	game.marking(player1, 0, 0)
+	game.marking(player2, 2, 2)
+	game.marking(player1, 0, 1)
+	game.marking(player2, 0, 2)
+	game.marking(player1, 1, 2)
+	game.marking(player2, 1, 1)
+	game.marking(player1, 2, 0)
+	game.marking(player2, 1, 0)
+	game.marking(player1, 2, 1)
+	actual := game.checkFullBoard()
+
+	if expected != actual {
+		t.Errorf("Expect %v but got %v", expected, actual)
+	}
+}
