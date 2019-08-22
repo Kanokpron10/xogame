e0736fd4802672d9f5a0b4fb1094d30b8e7acb2d
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-22 23:54:10 +0700

    [Added]: เพิ่มเทส Test_PlayFullBoard_Should_Be_Tie ในไฟล์ xogame_test.go

--- xogame/xogame_test.go
+++ xogame/xogame_test.go
@@ -158,3 +158,24 @@ func Test_PlayerTwo_Win_Horizontal_FirstLine(t *testing.T) {
 		t.Errorf("Expect %s but got %s", expected, actual)
 	}
 }
+
+func Test_PlayFullBoard_Should_Be_Tie(t *testing.T) {
+	expected := "Tie"
+	player1 := xogame.NewPlayer("Mo", "x")
+	player2 := xogame.NewPlayer("Praw", "o")
+	game := xogame.NewGame(player1, player2, "x")
+
+	game.Play(player1, 0, 0)
+	game.Play(player2, 2, 2)
+	game.Play(player1, 0, 1)
+	game.Play(player2, 0, 2)
+	game.Play(player1, 1, 2)
+	game.Play(player2, 1, 1)
+	game.Play(player1, 2, 0)
+	game.Play(player2, 1, 0)
+	actual := game.Play(player1, 2, 1)
+
+	if expected != actual {
+		t.Errorf("Expect %s but got %s", expected, actual)
+	}
+}
