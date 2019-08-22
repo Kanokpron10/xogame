918fe9c86ca5240484ca2bacab9543c6bafff417
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-22 14:30:16 +0700

    [Added]: เพิ่มเทส Test_PlayerOne_Marking_Position_0_0_And_Position_0_1 ในไฟล์ xogame_private_test.go

--- xogame/xogame_private_test.go
+++ xogame/xogame_private_test.go
@@ -17,3 +17,18 @@ func Test_PlayerOne_Marking_Position_0_0(t *testing.T) {
 		t.Errorf("Expect %v but got %v", expected, actual)
 	}
 }
+
+func Test_PlayerOne_Marking_Position_0_0_And_Position_0_1(t *testing.T) {
+	expected := [3][3]string{{"x", "x", ""}, {"", "", ""}, {"", "", ""}}
+	player1 := NewPlayer("Mo", "x")
+	player2 := NewPlayer("Praw", "o")
+	game := NewGame(player1, player2, "x")
+
+	game.marking(player1, 0, 0)
+	game.marking(player1, 0, 1)
+	actual := game.Board
+
+	if expected != actual {
+		t.Errorf("Expect %v but got %v", expected, actual)
+	}
+}
