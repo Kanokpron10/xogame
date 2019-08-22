75c8daba0466a16ecbe3e97b1c97730f31c68186
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-22 15:05:52 +0700

    [Edited]: แก้ไข expected เทส Test_PlayerOne_Win_Horizontal_Line_First ในไฟล์ xogame_test.go

--- xogame/xogame_test.go
+++ xogame/xogame_test.go
@@ -6,10 +6,10 @@ import (
 )
 
 func Test_PlayerOne_Win_Horizontal_Line_First(t *testing.T) {
-	expected := "x WINNER"
+	expected := "x WIN Horizontal first line"
 	player1 := xogame.NewPlayer("Mo", "x")
 	player2 := xogame.NewPlayer("Praw", "o")
-	game := xogame.NewGame(player1, player2, "")
+	game := xogame.NewGame(player1, player2, "x")
 
 	game.Play(player1, 0, 0)
 	game.Play(player2, 1, 0)
