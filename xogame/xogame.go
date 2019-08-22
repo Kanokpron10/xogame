fb5d1a1e9e843877306545af0f445991f48add00
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-22 14:48:05 +0700

    [Edited]: แก้ไขฟังก์ชัน checkWin ในไฟล์ xogame.go

--- xogame/xogame.go
+++ xogame/xogame.go
@@ -40,6 +40,11 @@ func (game *Game) marking(player Player, row, column int) {
 }
 
 func (game Game) checkWin() string {
+	if game.Board[0][0] == "x" &&
+		game.Board[0][1] == "x" &&
+		game.Board[0][2] == "x" {
+		return "WIN"
+	}
 	return "x WINNER"
 }
 
