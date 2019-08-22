7b6de3af232e78a4dc5cc6ac1816ec57a9706fd0
Kanokpron Thanawattanakun <kanokpron10@MacBook-Pro-khxng-Kanokpron.local>
2019-08-22 14:26:47 +0700

    [Created]: สร้างไฟล์ xogame_private_test.go

---
+++ xogame/xogame_private_test.go
@@ -0,0 +1,19 @@
+package xogame
+
+import (
+	"testing"
+)
+
+func Test_PlayerOne_Marking_Position_0_0(t *testing.T) {
+	expected := [3][3]string{{"x", "", ""}, {"", "", ""}, {"", "", ""}}
+	player1 := NewPlayer("Mo", "x")
+	player2 := NewPlayer("Praw", "o")
+	game := NewGame(player1, player2, "x")
+
+	game.marking(player1, 0, 0)
+	actual := game.Board
+
+	if expected != actual {
+		t.Errorf("Expect %v but got %v", expected, actual)
+	}
+}
